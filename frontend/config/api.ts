f5c2b5168fc39c00f0199f4b901aa5d08e155639
Lasso Sin Límites <brayanalex.ltg@gmail.com>
2026-05-02 13:51:59 +0200

    agregar el docker file correcto al frontend

--- frontend/config/api.ts
+++ frontend/config/api.ts
@@ -1,5 +1,4 @@
-export const STRAPI_URL =
-  process.env.NEXT_PUBLIC_STRAPI_URL || "http://localhost:1337";
+export const STRAPI_URL = process.env.NEXT_PUBLIC_STRAPI_URL;
 
 export function formatAssetUrl(url: string | null) {
   if (!url) return null;
